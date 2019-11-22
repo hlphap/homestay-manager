ddab15eab47d7ddfee7ac6ba466859069d783cbd
bumvaden12 <drevil800@gmail.com>
2019-11-22 13:30:29 +0700

    l

--- HomeStay/HomeStay/KhachDoan.cs
+++ HomeStay/HomeStay/KhachDoan.cs
@@ -12,9 +12,42 @@ namespace HomeStay
 {
     public partial class KhachDoan : UserControl
     {
+        private
+            TaoDoan_KhachDoan CtrTD = new TaoDoan_KhachDoan();
+            DoanDangO_KhachDoan CtrDDO = new DoanDangO_KhachDoan();
+            DoanDaDat_KhachDoan CtrDDDat = new DoanDaDat_KhachDoan();
+            DoanDaDi_KhachDoan CtrDDDi = new DoanDaDi_KhachDoan();
         public KhachDoan()
         {
             InitializeComponent();
+            PanelNoiDungKhachDoan.Controls.Add(CtrTD);
+            PanelNoiDungKhachDoan.Controls.Add(CtrDDO);
+            PanelNoiDungKhachDoan.Controls.Add(CtrDDDi);
+            PanelNoiDungKhachDoan.Controls.Add(CtrDDDat);
+        }
+
+        private void ButtonTD_Click(object sender, EventArgs e)
+        {
+            CtrTD.BringToFront();
+            CtrTD.Show();
+        }
+
+        private void ButtonDDO_Click(object sender, EventArgs e)
+        {
+            CtrDDO.BringToFront();
+            CtrDDO.Show();
+        }
+
+        private void ButtonDDDi_Click(object sender, EventArgs e)
+        {
+            CtrDDDi.BringToFront();
+            CtrDDDi.Show();
+        }
+
+        private void buttonDDDat_Click(object sender, EventArgs e)
+        {
+            CtrDDDat.BringToFront();
+            CtrDDDat.Show();
         }
     }
 }
--- HomeStay/HomeStay/KhachLe.cs
+++ HomeStay/HomeStay/KhachLe.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using HomeStay.Resources;
 
 namespace HomeStay
 {
@@ -14,11 +15,17 @@ namespace HomeStay
     {
         private
             TongHop_KhachLe CtrTH = new TongHop_KhachLe();
+            TuyChon_KhachLe CtrTC = new TuyChon_KhachLe();
+            KhachLe_DichVuMoRong CtrDVMR = new KhachLe_DichVuMoRong();
+            ThanhToan_KhachLe CtrTT = new ThanhToan_KhachLe();
         public KhachLe()
         {
             InitializeComponent();
             PanelNoiDungKhachLe.Controls.Add(CtrTH);
-     
+            PanelNoiDungKhachLe.Controls.Add(CtrTC);
+            PanelNoiDungKhachLe.Controls.Add(CtrDVMR);
+            PanelNoiDungKhachLe.Controls.Add(CtrTT);
+
         }
 
         private void bunifuCards1_Paint(object sender, PaintEventArgs e)
@@ -50,7 +57,20 @@ namespace HomeStay
 
         private void bunifuButton2_Click(object sender, EventArgs e)
         {
-            
+            CtrTC.BringToFront();
+            CtrTC.Show();
         }
+
+        private void Button_TuyChonMorong_Click(object sender, EventArgs e)
+        {
+            CtrDVMR.BringToFront();
+            CtrDVMR.Show();
+        }
+
+        private void ButtonThanhToan(object sender, EventArgs e)
+        {
+            CtrTT.BringToFront();
+            CtrTT.Show();
+        }     
     }
 }
