41a08ed9d7ae7014ca76292399b8be30bb79e579
phaptl <longphappy@gmail.com>
2019-11-29 23:11:37 +0700

    Revert "phap"
    This reverts commit 4ca7c2f25d364bbe9111dbba7c0019d9cf12e0a8.

--- HomeStay/HomeStay/FormChinh.cs
+++ HomeStay/HomeStay/FormChinh.cs
@@ -5,8 +5,8 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Threading; 
 
 namespace HomeStay
 {
@@ -26,26 +26,14 @@ namespace HomeStay
         public FormChinh()
         {
             InitializeComponent();
-            this.Load += new EventHandler(Form_Load); 
-        }
-
-        public void Form_Load(object sender, EventArgs e)
-        {
-            Thread newThread = new Thread(() =>
-            {
-                Invoke(new Action(() =>
-                {
-                    PanelNoiDung.Controls.Add(CtrTTC);
-                    PanelNoiDung.Controls.Add(CtrSDP);
-                    PanelNoiDung.Controls.Add(CtrLT);
-                    PanelNoiDung.Controls.Add(CtrKL);
-                    PanelNoiDung.Controls.Add(CtrKD);
-                    PanelNoiDung.Controls.Add(CtrTC);
-                    PanelNoiDung.Controls.Add(CtrTK);
-                    PanelNoiDung.Controls.Add(CtrBC);
-                })); 
-            });
-            newThread.Start(); 
+            PanelNoiDung.Controls.Add(CtrTTC);
+            PanelNoiDung.Controls.Add(CtrSDP);
+            PanelNoiDung.Controls.Add(CtrLT);
+            PanelNoiDung.Controls.Add(CtrKL);
+            PanelNoiDung.Controls.Add(CtrKD);
+            PanelNoiDung.Controls.Add(CtrTC);
+            PanelNoiDung.Controls.Add(CtrTK);
+            PanelNoiDung.Controls.Add(CtrBC);
         }
 
         private void ButtonExit_Click(object sender, EventArgs e)
@@ -56,6 +44,7 @@ namespace HomeStay
         private void ButtonThongTinChung_Click(object sender, EventArgs e)
         {
             CtrTTC.BringToFront();
+            Animator.ShowSync(CtrTTC);
         }
 
 
@@ -64,6 +53,7 @@ namespace HomeStay
         {
            
             CtrSDP.BringToFront();
+            Animator.ShowSync(CtrSDP);
         }
 
 
@@ -71,31 +61,37 @@ namespace HomeStay
         private void ButtonLeTan_Click(object sender, EventArgs e)
         {
             CtrLT.BringToFront();
+            Animator.ShowSync(CtrLT);
         }
 
         private void ButtonKhachLe_Click(object sender, EventArgs e)
         {
             CtrKL.BringToFront();
+            Animator.ShowSync(CtrKL);
         }
 
         private void ButtonKhachDoan_Click(object sender, EventArgs e)
         {
             CtrKD.BringToFront();
+            Animator.ShowSync(CtrKD);
         }
 
         private void ButtonThuChi_Click(object sender, EventArgs e)
         {
             CtrTC.BringToFront();
+            Animator.ShowSync(CtrTC);
         }
 
         private void ButtonThongKe_Click(object sender, EventArgs e)
         {
             CtrTK.BringToFront();
+            Animator.ShowSync(CtrTK);
         }
 
         private void ButtonBaoCao_Click(object sender, EventArgs e)
         {
             CtrBC.BringToFront();
+            Animator.ShowSync(CtrBC);
         }
 
         private void bunifuImageButton1_Click(object sender, EventArgs e)
