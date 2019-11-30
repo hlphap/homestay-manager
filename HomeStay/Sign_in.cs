a60ce378cb47a2e0d8c136857944b25285fd8338
tranhuywin <tranhuywin11b1@gmail.com>
2019-11-30 22:12:46 +0700

    tao tai khoan

--- HomeStay/Sign_in.cs
+++ HomeStay/Sign_in.cs
@@ -25,6 +25,7 @@ namespace HomeStay
         private void Button_Signin_Click(object sender, EventArgs e)
         {
             Form NewForm = new FormChinh();
+            // DATA SOURCE
             SqlConnection conn = new SqlConnection(@"Data Source = SE140697\SQLEXPRESS; Initial Catalog = HomeStay; Integrated Security = True");
             conn.Open();
             string Taikhoan = Textbox_Username.Text;
@@ -48,42 +49,21 @@ namespace HomeStay
             Application.Exit();
         }
 
-        private void Textbox_Username_TextChanged(object sender, EventArgs e)
-        {
-  
-        }
+
 
         private void Button_Shutdown_Click_1(object sender, EventArgs e)
         {
             Application.Exit();
         }
 
-        private void Button_Signin_DragEnter(object sender, DragEventArgs e)
-        {
-            Form NewForm = new FormChinh();
-            SqlConnection conn = new SqlConnection(@"Data Source = SE140697\SQLEXPRESS; Initial Catalog = HomeStay; Integrated Security = True");
-            conn.Open();
-            string Taikhoan = Textbox_Username.Text;
-            string MatKhau = Textbox_Password.Text;
-            string sql = "SELECT TaiKhoan, MatKhau FROM Sign_in WHERE TaiKhoan = '" + Taikhoan + "' AND MatKhau = '" + MatKhau + "'";
-            SqlCommand cmd = new SqlCommand(sql, conn);
-            SqlDataReader dta = cmd.ExecuteReader();
-            if (dta.Read() == true)
-            {
-                this.Hide();
-                NewForm.ShowDialog();
-            }
-            else
-            {
-                MessageBox.Show("Sai Mat Khau! Nhap Lai? ", "Loi", MessageBoxButtons.OK);
-            }
-        }
+
 
         private void Textbox_Password_KeyUp(object sender, KeyEventArgs e)
         {
             if(e.KeyCode == Keys.Enter)
             {
                 Form NewForm = new FormChinh();
+                // DATA SOURCE
                 SqlConnection conn = new SqlConnection(@"Data Source = SE140697\SQLEXPRESS; Initial Catalog = HomeStay; Integrated Security = True");
                 conn.Open();
                 string Taikhoan = Textbox_Username.Text;
@@ -101,6 +81,13 @@ namespace HomeStay
                     MessageBox.Show("Sai Mat Khau! Nhap Lai? ", "Loi", MessageBoxButtons.OK);
                 }
             }
+        }
+
+        private void bnt_tao_tai_khoan_Click(object sender, EventArgs e)
+        {
+            tao_tai_khoan taotaikhoan = new tao_tai_khoan();
+            this.Hide();
+            taotaikhoan.ShowDialog();
         }
     }
 }
