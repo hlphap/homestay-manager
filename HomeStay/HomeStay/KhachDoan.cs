84ce8a05e1ccf5ea301e4cc0dcbafee1a581ca44
phaptl <longphappy@gmail.com>
2020-01-04 09:15:32 +0700

    no message

--- HomeStay/HomeStay/KhachDoan.cs
+++ HomeStay/HomeStay/KhachDoan.cs
@@ -14,9 +14,7 @@ namespace HomeStay
     {
         private
             KhachDoan_TaoDoan CtrTD = new KhachDoan_TaoDoan();
-            KhachDoan_DoanDangO CtrDDO = new KhachDoan_DoanDangO();
-            KhachDoan_DoanDaDat CtrDDDat = new KhachDoan_DoanDaDat();
-            KhachDoan_DoanDaDi CtrDDDi = new KhachDoan_DoanDaDi();
+   
         public KhachDoan()
         {
             InitializeComponent();
@@ -30,9 +28,7 @@ namespace HomeStay
                 Invoke(new Action(() =>
                 {
                     PanelNoiDungKhachDoan.Controls.Add(CtrTD);
-                    PanelNoiDungKhachDoan.Controls.Add(CtrDDO);
-                    PanelNoiDungKhachDoan.Controls.Add(CtrDDDi);
-                    PanelNoiDungKhachDoan.Controls.Add(CtrDDDat);
+              
                 }));
             });
             newThread.Start();
@@ -42,25 +38,10 @@ namespace HomeStay
         {
             CtrTD.BringToFront();
             CtrTD.Show();
+
+
         }
 
-        private void ButtonDDO_Click(object sender, EventArgs e)
-        {
-            CtrDDO.BringToFront();
-            CtrDDO.Show();
-        }
-
-        private void ButtonDDDi_Click(object sender, EventArgs e)
-        {
-            CtrDDDi.BringToFront();
-            CtrDDDi.Show();
-        }
-
-        private void buttonDDDat_Click(object sender, EventArgs e)
-        {
-            CtrDDDat.BringToFront();
-            CtrDDDat.Show();
-        }
 
         
     }
